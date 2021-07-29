0dd43cd69c8701524c0341ae2c7874e965a20944
UtkarshA135 <utkarshagarwal101@gmail.com>
2021-07-29 12:55:52 +0530

    env updates

--- server.js
+++ server.js
@@ -1,39 +1,36 @@
-var express = require('express');
+var express = require("express");
 var app = express();
-var bodyParser = require('body-parser');
-app.use(bodyParser.json())
+var bodyParser = require("body-parser");
+app.use(bodyParser.json());
 
-let path = __dirname + '/build/';
-app.use(express.static('build'));
+let path = __dirname + "/build/";
+app.use(express.static("build"));
 // Configuring the database
-const dbConfig = require('./app/config/mongodb.config.js');
-const mongoose = require('mongoose');
+const dbConfig = require("./app/config/mongodb.config.js");
+const mongoose = require("mongoose");
 
-const Item = require('./app/models/item.model.js');
- 
+const Item = require("./app/models/item.model.js");
+
 mongoose.Promise = global.Promise;
- 
-// Connecting to the database
-mongoose.connect(dbConfig.url, { useNewUrlParser: true, useUnifiedTopology: true })
-    .then(async () => {
-        console.log("Successfully connected to MongoDB.");   
-     
-    }).catch(err => {
-        console.log(err);
-        console.log('Could not connect to MongoDB.');
-        process.exit();
-    });
-   
 
-    app.get('/', function (req,res) {
-        res.sendFile(path + "index.html");
-      });
-require('./app/routes/customer.router.js')(app);
+// Connecting to the database
+mongoose
+	.connect(dbConfig.url, { useNewUrlParser: true, useUnifiedTopology: true })
+	.then(async () => {
+		console.log("Successfully connected to MongoDB.");
+	})
+	.catch((err) => {
+		console.log(err);
+		console.log("Could not connect to MongoDB.");
+		process.exit();
+	});
+
+app.get("/", function (req, res) {
+	res.sendFile(path + "index.html");
+});
+require("./app/routes/customer.router.js")(app);
+
 // Create a Server
-var server = app.listen(8080 ||(process.env.PORT, '0.0.0.0')
-, function () { 
-  var host = server.address().address
-  var port = server.address().port || process.env.PORT
- 
-  console.log("App listening at http://%s:%s", host, port) 
-})
+app.listen(process.env.PORT || 8080, function () {
+	console.log("App listening at port: %s", port);
+});
